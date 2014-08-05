7ceaf6e41b43962de89dccf87ad4bf26ec154009
asbudik <asbudik@live.com>
2014-08-04 23:38:36 -0700

    Pull from Flickr API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express"),
   bodyParser = require("body-parser"),
+  request = require("request"),
   passport = require("passport"),
   twitterStrategy = require('passport-twitter').Strategy,
   passportLocal = require("passport-local"),
@@ -9,6 +10,8 @@ var express = require("express"),
   flash = require('connect-flash'),
   methodOverride = require('method-override'),
   morgan = require('morgan'),
+  _ = require('lodash'),
+  OAuth = require('oauth'),
   app = express();
 
 // Middleware for ejs, grabbing HTML and including static files
@@ -32,14 +35,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+// sign-in/log-in to twitter
+
 passport.use(new twitterStrategy ({
   consumerKey: process.env.TWITTER_KEY,
   consumerSecret: process.env.TWITTER_SECRET,
   callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
 },
 function(accessToken, tokenSecret, profile, done) {
-  // console.log(profile);
-//  process.nextTick(function () {
+
   db.user.findOrCreate({username: profile.username,
     twitterid: profile.id, accesstoken: accessToken, 
     tokensecret: tokenSecret}).success(function(user, created) {
@@ -47,9 +51,11 @@ function(accessToken, tokenSecret, profile, done) {
       console.log("created", created);
       done(null, user);
   })
-// })
+
 return 
 }))
+
+// authenticate twitter credentials
 
 app.get('/auth/twitter',
 passport.authenticate('twitter'),
@@ -85,8 +91,30 @@ app.get('/', function(req, res) {
   res.render("index", {isAuthenticated: req.isAuthenticated()});
 })
 
+app.get('/search', function(req, res) {)
+  var oauth = new OAuth.OAuth(
+    'https://api.flickr.com/oauth/request_token',
+    'https://api.flickr.com/oauth/access_token',
+    process.env.FLICKR_KEY,
+    process.env.FLICKR_SECRET,
+    '1.0A',
+    null,
+    'HMAC-SHA1'
+  );
 
+  var searchURL ="https://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&tags=rainbow&api_key=" + process.env.FLICKR_KEY + "&nojsoncallback=1";
 
+  oauth.get(searchURL, null, null, function(e, data, res) {
+    // console.log(e);
+    var flickr = JSON.parse(data)
+    var photostuff = flickr.photos.photo
+    console.log(photostuff);
+    // console.log(res);
+  })
+  res.render('search', {isAuthenticated: req.isAuthenticated(),
+  photostuff: photostuff}
+  })
+})
 
 app.get('/users', function(req, res) {
   db.user.findAll({order: [['createdAt', 'DESC']]}).success(function(allUsers) {
@@ -95,9 +123,11 @@ app.get('/users', function(req, res) {
   });
 });
 
-app.get('/search', function(req, res) {
-  res.render("search", {isAuthenticated: req.isAuthenticated()});
-})
+
+
+// app.get('/search', function(req, res) {
+//   res.render("search", {isAuthenticated: req.isAuthenticated()});
+// })
 
 app.get('/logout', function(req,res){
   req.logout();
