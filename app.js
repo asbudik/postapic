0344ce911cc6c4ff4d2db0d9fa952f607ad7b8bb
asbudik <asbudik@live.com>
2014-08-05 22:59:36 -0700

    quick refactor, will clean up towards the end

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
+// OAuth
 var oauth = new OAuth.OAuth(
   "req.user.accesstoken",
   "req.user.tokensecret",
@@ -46,6 +47,7 @@ var oauth = new OAuth.OAuth(
   'HMAC-SHA1'
 );
 
+// Login with Twitter
 passport.use(new twitterStrategy ({
   consumerKey: process.env.TWITTER_KEY,
   consumerSecret: process.env.TWITTER_SECRET,
@@ -91,30 +93,15 @@ passport.deserializeUser(function(id, done) {
   })
 })
 
-app.get('/', function(req, res) {
-  res.render("index", {isAuthenticated: req.isAuthenticated()});
-})
 
-// function for Math.rand which will assist in returning a
-// single, randomized photo from a search query.
+// ***END BOILERPLATE***
+
+
 function getRandomInt(min, max) {
+  // function for Math.rand which will assist in returning a
+  // single, randomized photo from a search query.
   return Math.floor(Math.random() * (max - min)) + min;
 }
-
-function imageRequest(dataInput, imageValue)
-
-      var bodyData = JSON.parse(body);
-      var data = bodyData.dataInput;
-      console.log(bodyData)
-
-      // from an array of objects, randomly select one
-      var foundPhoto = data[getRandomInt(0, data.length-1)].imageValue;
-      res.render("result", {isAuthenticated: req.isAuthenticated(),
-      foundPhoto: foundPhoto})
-    }
-  })
-}
-
 
 app.get('/result', function(req, res) {
   if (req.query.searchpic === 'stock') {
@@ -122,27 +109,26 @@ app.get('/result', function(req, res) {
     + req.query.searchTerm + "&api_key=" + process.env.FLICKR_KEY 
     + "&nojsoncallback=1&media=photos&extras=url_m&page=1&per_page=500&tag_mode=all";
 
-    console.log(req.query.searchTerm)
     request(searchURL, function(error, response, body) {
       if(!error) {
-        imageRequest(photos.photo, url_m)
-        
+
+        var bodyData = JSON.parse(body);
+        var data = bodyData.photos.photo;
+        var foundPhoto = data[getRandomInt(0, data.length-1)].url_m;
+
+        res.render("result", {isAuthenticated: req.isAuthenticated(),
+        foundPhoto: foundPhoto})
       }
     })
   } else {
     var memeURL = "http://version1.api.memegenerator.net/Generators_Search/?q=" + req.query.searchTerm + "&pageSize=24";
-    console.log(req.query.searchTerm)
-    console.log(req.query.searchpic)
     request(memeURL, function(error, response, body) {
       if(!error) {
 
-        console.log(memeURL)
         var bodyData = JSON.parse(body);
         var data = bodyData.result;
-
-        // from an array of objects, randomly select one
         var foundPhoto = data[getRandomInt(0, data.length-1)].imageUrl;
-        console.log(foundPhoto)
+
         res.render("result", {isAuthenticated: req.isAuthenticated(),
         foundPhoto: foundPhoto})
       }
@@ -150,19 +136,13 @@ app.get('/result', function(req, res) {
   }
 })
 
-// var searchURL ="https://api.twitter.com/1.1/search/tweets.json?q=coffee&result_type=recent&lang=en";
-
-// oauth.get(searchURL, null, null, function(e, data, res) {
-//   // console.log(e);
-//   var tweets = JSON.parse(data).statuses
-//   var tweetText = _.pluck(tweets, "text")
-//   console.log(tweetText);
-//   // console.log(res);
-// })
+app.get('/', function(req, res) {
+  res.render("index", {isAuthenticated: req.isAuthenticated()});
+});
 
 app.get('/search', function(req, res) {
   res.render("search", {isAuthenticated: req.isAuthenticated()});
-})
+});
 
 app.get('/users', function(req, res) {
   db.user.findAll({order: [['createdAt', 'DESC']]}).success(function(allUsers) {
@@ -178,11 +158,11 @@ app.get('/logout', function(req,res){
 
 app.get('*', function(req, res) {
   res.render("error", { isAuthenticated: req.isAuthenticated()});
-})
+});
 
 app.listen(3000, function() {
   console.log("Running");
-})
+});
 
 
 
